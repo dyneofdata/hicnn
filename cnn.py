a355badfba43a145c6f9dd6ce37ad64b91bf5362
Da-Inn Lee <dyne0102@gmail.com>
2017-04-25 19:57:53 -0500

    04/25/2017 Raw DNA input + multi-input CNN

--- cnn.py
+++ cnn.py
@@ -14,6 +14,8 @@ from keras.layers.advanced_activations import ELU
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger, LearningRateScheduler
 from keras.optimizers import Adam
 import plot
+import bitstring
+from string import maketrans
 
 def step_decay(epoch):
 	initial_lrate = 0.01
@@ -22,54 +24,54 @@ def step_decay(epoch):
 	lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
 	return lrate
 
-## Input & outpus:
+## Input & outputs:
 chro = sys.argv[1]
 cell = sys.argv[2]
 fold = sys.argv[3]
 mode = sys.argv[4]
 
-input_seq = 'input/chr'+ chro + '/sequence.txt'
+input_seq = 'input/chr'+ chro + '/raw_sequence.txt'
 input_chr = 'input/chr'+ chro + '/' + cell + '/markers.txt'
 input_train = 'input/chr'+ chro + '/' + cell + '/train' + fold + '.txt'
 input_test = 'input/chr'+ chro + '/' + cell + '/test' + fold + '.txt'
 output_name = 'output/chr' + chro + '_' + cell + '_f' + fold + '_' + mode
 image_title = 'Chromosome ' + chro + ' ' + cell + ' (Fold=' + fold + ')'
 
-neighbors = 7
-maxLength = (neighbors * 2 + 1) * 2
-
-## Sequence data & chromatin marker data (features):
+## Sequence data (features):
 with open(input_seq) as seqData:
 	reader = csv.reader(seqData, delimiter= '\t')
 	seqData = list(reader)
 seqData.pop(0)
-'''
-with open(input_chr) as chrData:
-	reader = csv.reader(chrData, delimiter= '\t')
-	chrData = list(reader)
-chrData.pop(0)
+numRegions = 50000
+shape = (numRegions, 625)
+print shape
+A = np.zeros(shape = shape)
+C = np.zeros(shape = shape)
+T = np.zeros(shape = shape)
+G = np.zeros(shape = shape)
 
-numRegions = len(seqData) if len(seqData) > len(chrData) else len(chrData)
+o = 'AaTtCcGgN'
+a = '110000000'
+t = '001100000'
+c = '000011000'
+g = '000000110'
+l = [a, t, c, g]
 
-seq = np.zeros(shape=(numRegions,len(seqData[0])-1))
 for region in seqData:
-	seq[int(region[0])] = [float(i) for i in region[1:]]
-
-chrom = np.zeros(shape=(numRegions,len(chrData[0])-1))
-for region in chrData:
-	chrom[int(region[0])] = [float(i) for i in region[1:]]
-
-chrom = chrom / chrom.max(axis = 0)
-
-features = np.concatenate((seq, chrom), axis = 1)
-'''
-numRegions = len(seqData)
-seq = np.zeros(shape=(numRegions,len(seqData[0])-1))
-for region in seqData:
-	seq[int(region[0])] = [float(i) for i in region[1:]]
-features = seq
-numChannels = len(features[0])
-pad = np.zeros(shape = (1, int(numChannels)))
+	idx = int(int(region[0].split('_')[1])/5000)
+	for nt, out in enumerate(l):
+		trans = maketrans(o, out)
+		s = region[1][:-2].translate(trans)
+		b = bitstring.BitArray(bin = s)
+		i = [float(ord(x)) for x in b.tobytes()]
+		if nt == 0:
+			A[idx] = i
+		elif nt == 1:
+			T[idx] = i
+		elif nt == 2:
+			C[idx] = i
+		else:
+			G[idx] = i 
 
 ## Train data:
 with open(input_train) as trainData:
@@ -78,85 +80,67 @@ with open(input_train) as trainData:
 trainData.pop(0)
 numExamples = int(len(trainData))
 	
-shape = [numExamples] + [maxLength] + [numChannels]
-X = np.zeros(shape = shape)
+shape = [numExamples] + [625] + [4]
+X1 = np.zeros(shape = shape)
+X2 = np.zeros(shape = shape)
 Y = np.array(trainData)[:,2].astype(float)
 
 for idx, example in enumerate(trainData):
         e = int(example[0])
         p = int(example[1])     
-        e_start = 0 if (e - neighbors) < 0 else e - neighbors
-        e_end = numRegions - 1 if (e + neighbors) >= numRegions else e + neighbors
-        p_start = 0 if (p - neighbors) < 0 else p - neighbors
-        p_end = numRegions - 1 if (p + neighbors) >= numRegions else p + neighbors 
-        e_front_pad = -1 * (e - neighbors)
-        e_back_pad  = (e + neighbors) - numRegions + 1 if (e + neighbors) >= numRegions else 0
-        p_front_pad = -1 * (p - neighbors)
-        p_back_pad  = (p + neighbors) - numRegions + 1 if (p + neighbors) >= numRegions else 0
-        if e_front_pad > 0:
-                E = np.concatenate((np.repeat(pad, e_front_pad, axis = 0), features[e_start:e_end+1,:]), axis=0)
-        else:
-                E = features[e_start:e_end+1,:]
-        if e_back_pad > 0:
-                E = np.concatenate((E, np.repeat(pad, e_back_pad, axis = 0)), axis = 0)
-        if p_front_pad > 0:
-                E = np.concatenate((np.repeat(pad, p_front_pad, axis = 0), features[p_start:p_end+1,:]), axis=0)
-        else:
-                P = features[p_start:p_end+1,:]
-        if p_back_pad > 0:
-                P = np.concatenate((P, np.repeat(pad, p_back_pad, axis = 0)), axis = 0)
-        x = np.concatenate((E,P), axis = 0)
-        X[idx] = x
+	X1[idx] = np.array((A[e], C[e], G[e], T[e])).T
+	X2[idx] = np.array((A[p], C[p], G[p], T[p])).T
 
 ## Model:
-if mode == 'vanilla':
-        shape = [maxLength] + [numChannels]
-        model = Sequential()
-        model.add(Conv1D(input_shape = shape, filters = 10, kernel_size = 4, strides = 1, padding = 'same', activation = 'relu', use_bias = True, kernel_initializer = 'glorot_uniform', bias_initializer = 'glorot_uniform'))
-        model.add(MaxPooling1D(padding='same'))
-        model.add(Dropout(0.5))
-        model.add(Conv1D(filters = 10, kernel_size = 4, strides = 1, padding = 'same', activation = 'relu', use_bias = True, kernel_initializer = 'glorot_uniform', bias_initializer = 'glorot_uniform'))
-        model.add(MaxPooling1D(padding='same'))
-        model.add(Dropout(0.5))
-        model.add(Flatten())
-        model.add(Dense(units = 50, activation = 'relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(units = 1, activation = 'linear'))
-        model.compile(loss='mean_squared_error', optimizer = 'Adam')
-else:
-        filter_sizes = (3, 7)
-        num_filters = 10
-        dropout_prob = 0.5
-        hidden_dims = 50
-        shape = [maxLength] + [numChannels]
+num_filters = 10
+kernel_size = 3
+dropout_prob = 0.5
+hidden_dims = 50
+shape = [625] + [4] 
 
-        model_input = Input(shape=shape)
-        conv_blocks = []
-        for sz in filter_sizes:
-            conv = Conv1D(filters=num_filters,
-                                 kernel_size=sz,
-                                 padding="valid",
-                                 strides=1)(model_input)
-            conv = ELU()(conv)
-            conv = MaxPooling1D(pool_size=2)(conv)
-            conv = Flatten()(conv)
-            conv_blocks.append(conv)
-        z = Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
-        z = Dropout(dropout_prob)(z)
-        z = Dense(hidden_dims)(z)
-        z = ELU()(z)
-        model_output = Dense(1)(z)
-        model = Model(model_input, model_output)
+# input layer
+input1 = Input(shape=shape, name='input1')
+input2 = Input(shape=shape, name='input2')
 
-        adam = Adam(lr = 0.01)
-        model.compile(loss='mean_squared_error', optimizer = 'adam')
+# convolution layer for input1
+conv1 = Conv1D(filters=num_filters,
+				kernel_size=kernel_size,
+				padding="valid",
+				strides=1)(input1)
+conv1 = ELU()(conv1)
 
+# convolution layer for input2
+conv2 = Conv1D(filters=num_filters,
+				kernel_size=kernel_size,
+				padding="valid",
+				strides=1)(input2)
+conv2 = ELU()(conv2)
+
+# separate pooling layers for two inputs
+conv1 = MaxPooling1D(pool_size=2)(conv1)
+conv2 = MaxPooling1D(pool_size=2)(conv2)
+
+conv1 = Flatten()(conv1)
+conv2 = Flatten()(conv2)
+
+# merge two streams
+z = Concatenate()([conv1, conv2])
+z = Dropout(dropout_prob)(z)
+z = Dense(hidden_dims)(z)
+z = ELU()(z)
+model_output = Dense(1)(z)
+
+# fit model
 early_stopping = EarlyStopping(monitor='val_loss', patience = 10, verbose = 1)
 csv_logger = CSVLogger(output_name + '.log')
 checkpoint = ModelCheckpoint(output_name + '.hdf5', monitor = 'val_loss', verbose = 0, save_best_only = True)
 lrate = LearningRateScheduler(step_decay)
 
-model.fit(X, Y, batch_size = 32, epochs = 100, verbose = 2, callbacks = [early_stopping, lrate, csv_logger, checkpoint], validation_split = 0.1, shuffle = True)
+model = Model(inputs=[input1, input2], outputs=[model_output])
+adam = Adam(lr = 0.01)
+model.compile(loss='mean_squared_error', optimizer = 'adam')
+
+model.fit([X1, X2], Y, batch_size = 32, epochs = 100, verbose = 2, callbacks = [early_stopping, lrate, csv_logger, checkpoint], validation_split = 0.1, shuffle = True)
 
 ## Test:
 with open(input_test) as testData:
@@ -166,41 +150,24 @@ testData.pop(0)
 numExamples = int(len(testData))
 testData = sorted(testData, key = lambda x: int(x[3]))
 pairDistance = np.array(testData)[:,3].astype(int)
-shape = [numExamples] + [maxLength] + [numChannels]
-X = np.zeros(shape = shape)
+
+shape = [numExamples] + [625] + [4]
+X1 = np.zeros(shape = shape)
+X2 = np.zeros(shape = shape)
 Y_actual = np.array(testData)[:,2].astype(float)
 Y_ripple = np.array(testData)[:,4].astype(float)
 
 for idx, example in enumerate(testData):
         e = int(example[0])
-        p = int(example[1])     
-        e_start = 0 if (e - neighbors) < 0 else e - neighbors
-        e_end = numRegions - 1 if (e + neighbors) >= numRegions else e + neighbors
-        p_start = 0 if (p - neighbors) < 0 else p - neighbors
-        p_end = numRegions - 1 if (p + neighbors) >= numRegions else p + neighbors 
-        e_front_pad = -1 * (e - neighbors)
-        e_back_pad  = (e + neighbors) - numRegions + 1 if (e + neighbors) >= numRegions else 0
-        p_front_pad = -1 * (p - neighbors)
-        p_back_pad  = (p + neighbors) - numRegions + 1 if (p + neighbors) >= numRegions else 0
-        if e_front_pad > 0:
-                E = np.concatenate((np.repeat(pad, e_front_pad, axis = 0), features[e_start:e_end+1,:]), axis=0)
-        else:
-                E = features[e_start:e_end+1,:]
-        if e_back_pad > 0:
-                E = np.concatenate((E, np.repeat(pad, e_back_pad, axis = 0)), axis = 0)
-        if p_front_pad > 0:
-                E = np.concatenate((np.repeat(pad, p_front_pad, axis = 0), features[p_start:p_end+1,:]), axis=0)
-        else:
-                P = features[p_start:p_end+1,:]
-        if p_back_pad > 0:
-                P = np.concatenate((P, np.repeat(pad, p_back_pad, axis = 0)), axis = 0)
-        x = np.concatenate((E,P), axis = 0)
-        X[idx] = x
+	p = int(example[1])
+	X1[idx] = np.array((A[e], C[e], G[e], T[e])).T
+	X2[idx] = np.array((A[p], C[p], G[p], T[p])).T
 
 model = load_model(output_name + '.hdf5')
-Y_predict = model.predict(X)
+Y_predict = model.predict([X1, X2])
 Y_predict = Y_predict[:,0]
 
+# Output:
 plot.plotPearsonCoefficient(pairDistance, Y_actual, Y_predict, Y_ripple, output_name, image_title)
 plot.plotScatter(pairDistance, Y_actual, Y_predict, output_name, image_title)
 plot.plotMeanStdev(pairDistance, Y_actual, Y_predict, output_name, image_title)
